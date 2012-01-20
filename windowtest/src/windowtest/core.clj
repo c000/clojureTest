3f825398f3633a4dbde2c0340b750edb3000ed07
LabArchCore2 <sat@C000Arch.localdomain>
2012-01-20 16:21:43 +0900

    GridLayout Text

--- windowtest/src/windowtest/core.clj
+++ windowtest/src/windowtest/core.clj
@@ -1,7 +1,7 @@
 (ns windowtest.core
-  (:import [org.eclipse.swt.widgets Display Shell FileDialog Table TableColumn TableItem]
-           [org.eclipse.swt.events KeyAdapter]
-           [org.eclipse.swt.layout FillLayout]
+  (:import [org.eclipse.swt.widgets Display Shell FileDialog Table TableColumn TableItem Text]
+           [org.eclipse.swt.events KeyAdapter ModifyListener]
+           [org.eclipse.swt.layout GridLayout GridData]
            [org.eclipse.swt SWT]))
 
 (defn window-open []
@@ -9,13 +9,15 @@
         shell (Shell. display)]
     (doto shell
       (.setText "Hello")
-      (.setSize 640 480)
-      (.setLayout (FillLayout.))
+      (.setLayout 
+        (let [l (GridLayout. 1 true)]
+          (set! (.verticalSpacing l) 10)
+          l))
       (.addKeyListener
         (proxy [KeyAdapter] []
           (keyPressed [e]
                       (if (= (.character e) \q) (.close shell))))))
-    (let [t (Table. shell (bit-or SWT/CHECK SWT/FULL_SELECTION))]
+    (let [t (Table. shell (bit-or SWT/BORDER_DASH SWT/FULL_SELECTION))]
       (doseq [x ["hoge" "piyo" "foo"]]
         (doto (TableColumn. t SWT/LEFT)
           (.setText x)
@@ -24,11 +26,18 @@
         (doto (TableItem. t SWT/NULL)
           (.setText (into-array String (map (partial str d) ["a" "b" "c"])))))
       (.setHeaderVisible t true)
-      (.selectAll t))
+      (.selectAll t)
+      (.setLayoutData t (GridData. GridData/FILL_BOTH)))
+    (let [t (Text. shell (bit-or SWT/SINGLE SWT/BORDER))]
+      (.setLayoutData t (GridData. GridData/FILL_HORIZONTAL))
+      (.addModifyListener t
+        (proxy [ModifyListener] []
+          (modifyText [_]
+            (.setText shell (.getText t))))))
     ;(println
     ;  (-> (FileDialog. shell SWT/OPEN)
     ;    (.open)))
-    (.pack shell)
+    (.setSize shell 640 480)
     (.open shell)
     (doseq [_ (take-while (partial = false) (repeatedly #(.isDisposed shell)))]
       (if-not (.readAndDispatch display) (.sleep display)))
